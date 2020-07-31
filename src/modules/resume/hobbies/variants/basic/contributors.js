a88afb808035cb08f04ddae5ae1f21f14c912cba
Sakshamkapur <sskapur7@gmail.com>
2020-08-01 02:04:16 +0530

    added my contributor card

--- src/modules/resume/hobbies/variants/basic/contributors.js
+++ src/modules/resume/hobbies/variants/basic/contributors.js
@@ -3,6 +3,11 @@ const contributors = [
     name: 'Rahul Goyal',
     email: 'rahulgoyal7480@gmail.com',
     url: 'https://github.com/rahulgoyal21'
+  },
+  {
+    name: 'Saksham Kapur',
+    email: 'sskapur7@gmail.com',
+    url: 'https://github.com/Sakshamkapur'
   }
 ];
 
